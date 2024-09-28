e32b8278332e16405c2b0fe00d06ab2f028eb9bf
Alexander Boulos <alexander.boulos10@gmail.com>
2024-09-28 13:16:16 -0400

    added websocket endpoint

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,13 @@ class ConnectionManager:
             
 manager = ConnectionManager()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Websockets!"}
+@app.websocket("/ws/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Client says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client left the chat")
